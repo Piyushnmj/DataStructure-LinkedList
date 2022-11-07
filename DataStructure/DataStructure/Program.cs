fb1738468c3a5850fdfc1554056a6ef811e0470d
Piyushnmj <piyushnmj@yahoo.com>
2022-11-07 22:33:19 +0530

    Welcome

---
+++ DataStructure/DataStructure/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace DataStructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to LinkedList");
+            Console.ReadLine();
+        }
+    }
+}
