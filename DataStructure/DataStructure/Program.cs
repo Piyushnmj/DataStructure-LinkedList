77f278d01488ff8d53267be7ae4fdec5f353e842
Piyushnmj <piyushnmj@yahoo.com>
2022-11-08 16:00:19 +0530

    Create Linked List by adding the numbers to it

--- DataStructure/DataStructure/Program.cs
+++ DataStructure/DataStructure/Program.cs
@@ -7,10 +7,10 @@
             Console.WriteLine("Welcome to LinkedList");
 
             LinkedList objLinkedList = new LinkedList();
-            objLinkedList.Add(56);
-            objLinkedList.Add(30);
             objLinkedList.Add(70);
-            objLinkedList.Display(); // 56 30 70
+            objLinkedList.InsertFront(30);
+            objLinkedList.InsertFront(56);
+            objLinkedList.Display();
 
             Console.ReadLine();
         }
